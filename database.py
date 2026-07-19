540654f1be7787424448da097c35b4cd73e0c42e
norroin <dima06101980f@gmail.com>
2026-07-19 12:38:02 +0300

    Fix database pool

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import os
-
 import psycopg
 from psycopg_pool import ConnectionPool
 from dotenv import load_dotenv
@@ -12,61 +11,8 @@ pool = ConnectionPool(
     conninfo=DATABASE_URL,
     min_size=1,
     max_size=5,
-    open=True
 )
 
 db = pool.getconn()
 db.autocommit = True
 cur = db.cursor()
-
-
-def reconnect():
-    global db, cur
-
-    try:
-        cur.close()
-    except Exception:
-        pass
-
-    try:
-        pool.putconn(db)
-    except Exception:
-        pass
-
-    db = pool.getconn()
-    db.autocommit = True
-    cur = db.cursor()
-
-
-class SafeCursor:
-    def execute(self, query, params=None):
-        global cur
-
-        try:
-            return cur.execute(query, params)
-        except psycopg.OperationalError:
-            reconnect()
-            return cur.execute(query, params)
-
-    def fetchone(self):
-        return cur.fetchone()
-
-    def fetchall(self):
-        return cur.fetchall()
-
-    def __getattr__(self, name):
-        return getattr(cur, name)
-
-
-cur = SafeCursor()
-
-
-def close():
-    global db
-
-    try:
-        pool.putconn(db)
-    except Exception:
-        pass
-
-    pool.close()
