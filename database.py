b803307e11bd7f3b847c3770bcff0a1a8d9b082c
norroin <dima06101980f@gmail.com>
2026-07-19 17:57:25 +0300

    Fix PostgreSQL reconnect

--- database.py
+++ database.py
@@ -13,6 +13,52 @@ pool = ConnectionPool(
     max_size=5,
 )
 
-db = pool.getconn()
-db.autocommit = True
-cur = db.cursor()
+db = None
+cur = None
+
+
+def reconnect():
+    global db, cur
+
+    if db:
+        try:
+            pool.putconn(db)
+        except:
+            pass
+
+    db = pool.getconn()
+    db.autocommit = True
+    cur = db.cursor()
+
+
+reconnect()
+
+
+def execute(query, params=None):
+    global db, cur
+
+    try:
+        cur.execute(query, params or ())
+    except psycopg.OperationalError:
+        reconnect()
+        cur.execute(query, params or ())
+
+    return cur
+
+
+class SafeCursor:
+    def execute(self, query, params=None):
+        return execute(query, params)
+
+    def fetchone(self):
+        return cur.fetchone()
+
+    def fetchall(self):
+        return cur.fetchall()
+
+
+cur = SafeCursor()
+
+
+def commit():
+    pass
